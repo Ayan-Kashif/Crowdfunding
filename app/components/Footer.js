e21b183d861ab6d141ce435cdad3cdec968bc775
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-02-27 01:07:52 +0500

    Update Footer.js
    Updated Name

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
                         <div class="mb-6 md:mb-0">
                             <a href="https://flowbite.com/" class="flex items-center">
                                 <img src="images.png" class="h-8 me-3 rounded-full" alt="FlowBite Logo" />
-                                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FundMe</span>
+                                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Fundify</span>
                             </a>
                         </div>
                         <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
