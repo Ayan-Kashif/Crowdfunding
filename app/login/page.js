90c13b234282841eaebca06583df1778782c41e0
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-02-28 22:14:17 +0500

    Update page.js
    url changed.

--- app/login/page.js
+++ app/login/page.js
@@ -26,7 +26,7 @@ export default function Login() {
         setLoading(true);
 
         try {
-            const response = await fetch("http://localhost:5000/login", {
+            const response = await fetch("http://82.29.153.135:3000/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
