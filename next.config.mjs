97447b94a80446bbac77a8d4961938289d15fdfe
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-03-02 08:18:48 +0500

    Update next.config.mjs
    added stripe font

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://trusted-source.com;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
               img-src 'self' data: https://trusted-image-source.com;
-              font-src 'self' https://fonts.gstatic.com;
+              font-src 'self' https://fonts.gstatic.com https://js.stripe.com;
               connect-src 'self' https://api.yourbackend.com;
               frame-src 'self' https://trusted-iframe.com;
             `.replace(/\n/g, ""), // Remove new lines
@@ -24,3 +24,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
