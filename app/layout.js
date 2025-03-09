7daf2d3bd1b3e713212169db031e3beb12126c34
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-03-09 13:48:13 +0500

    Update layout.js
    updated layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { headers } from 'next/headers';
 import AuthProvider from '../app/AuthContext'
+import Head from "next/head";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -15,19 +17,31 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Fundify",
-  description: "Empowering small investors to support innovative projects, businesses, and ideas in a way that reflects our shared values and traditions.",
+  description: "The ultimate crowdfunding platform",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const requestHeaders = await headers(); // Get headers in a server component
+  const isAdminRoute = requestHeaders.get('is-admin-route') === 'true';
   return (
     <html lang="en">
+      <Head>
+       
+
+        <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Martel+Sans:wght@200;300;400;600;700;800;900&family=Playwrite+IT+Moderna:wght@100..400&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Martel+Sans:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet" />
+
+
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-        <Navbar/>
-        {children}
-        <Footer/>
+          {!isAdminRoute && <Navbar />}
+          {children}
+          <Footer />
         </AuthProvider>
       </body>
     </html>
