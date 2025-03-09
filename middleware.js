69d5b889e5071b4d505d3a25e043be0ff2941ce6
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-03-09 15:08:33 +0500

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  const pathname = request.nextUrl.pathname;
+ 
+
+  // Check if the current path starts with "/admin"
+  const isAdminRoute = pathname.startsWith('/admin');
+    console.log("Middleware is running on:", pathname); // Debug locally
+;
+
+
+  // Add the admin status to headers
+  const response = NextResponse.next();
+  response.headers.set('is-admin-route', (isAdminRoute  )? 'true' : 'false');
+
+  return response;
+}
